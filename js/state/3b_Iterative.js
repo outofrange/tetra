e686d650db47960c3c15e79e8a85cd3f1831fc53
Markus Möslinger <markus.moeslinger@gmail.com>
2016-04-28 07:34:36 +0100

    - loading player

--- js/state/3b_Iterative.js
+++ js/state/3b_Iterative.js
@@ -1,12 +1,23 @@
 Tetra.Iterative = function () {
     var field = new Tetra.Field(8, 0, 10, 39, 'map');
+    var config = new Tetra.Game.DefaultConfig();
 
-    Tetra.Game.call(this, field, Tetra.Iterative.Player);
+    Tetra.Game.call(this, field, Tetra.Iterative.getPlayer(), config);
 };
 
 
 Tetra.Iterative.prototype = Object.create(Tetra.Game.prototype);
 Tetra.Iterative.prototype.constructor = Tetra.Game;
+
+Tetra.Iterative.getPlayer = function () {
+    var player = localStorage.getItem('iterative.player');
+
+    if (player) {
+        return JSON.parse(player);
+    } else {
+        return Tetra.Iterative.Player;
+    }
+};
 
 Tetra.Iterative.Player = {
     name: getPlayerName(),
